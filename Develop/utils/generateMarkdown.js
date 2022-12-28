679ecf0813ad609faa36ce19cd6ceefeb3d9dc8c
Jacob Pickron <jpickron30@gmail.com>
2022-12-27 19:05:08 -0700

    added license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-
+function renderLicenseBadge(license) {
+  if (license === "MIT"){
+    return`![MIT](https://img.shields.io/badge/License-MIT-red)`
+  }else if(license === "Apache"){
+    return`![Apache](https://img.shields.io/badge/License-Apache-red)`
+  } else if(license === "GNU"){
+    return`![GNU](https://img.shields.io/badge/License-GNU-red)`
+  }else {
+    return``
+  }
+}
 
 
 // TODO: Create a function that returns the license link
