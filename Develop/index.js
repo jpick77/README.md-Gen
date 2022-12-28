18e701ac47264e288ea25388640334ec06a0a2cf
Jacob Pickron <jpickron30@gmail.com>
2022-12-27 17:13:31 -0700

    added the questions for the readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,5 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer");
-const fs = require("fs")
 
-const generateMarkdown = require("./utils/generateMarkdown.js");
-const { error } = require("console");
 // TODO: Create an array of questions for user input
 const questions = [
     {
